6b1c313f469ba819d74a01c1bb2315c4fb2b7e66
Yariv <shenkar7@gmail.com>
2021-04-28 16:12:08 +0300

    test commit

--- awaygo/settings.py
+++ awaygo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r+us(4yl@o1i_cx56&=zt-k9#g=u-^0w#2cmv+86498fi#p8f&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-env.eba-skrufx2d.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
