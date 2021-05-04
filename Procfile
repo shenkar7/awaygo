893574e01b4209c78bc8cc4558843b790b5693f3
Yariv <shenkar7@gmail.com>
2021-05-04 21:34:54 +0300

    installing django-heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn awaygo.wsgi 
+web: gunicorn awaygo.wsgi
--- awaygo/settings.py
+++ awaygo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'awaygoApp/staticfiles')
 
 STATIC_URL = '/static/'
 
@@ -141,3 +142,4 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
 
+django_heroku.settings(locals())
