e81477a5c332b6f06665167c895d919ee7243999
Yariv <shenkar7@gmail.com>
2021-05-05 23:01:55 +0300

    removing django-heroku package

--- awaygo/settings.py
+++ awaygo/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,4 +146,4 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
