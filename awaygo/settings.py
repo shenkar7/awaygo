8ecaafdb4bd56f63c84fbe517d83e9a866002870
Yariv <shenkar7@gmail.com>
2021-05-06 22:25:32 +0300

    ALLOWED_HOSTS update

--- awaygo/settings.py
+++ awaygo/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['awaygo.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['awaygo.herokuapp.com', 'awaygo2.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
