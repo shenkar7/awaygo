7be97636160818bda39b241a5b3e62645fd9f117
Yariv <shenkar7@gmail.com>
2021-05-08 22:48:06 +0300

    aws s3 media storage

--- awaygo/settings.py
+++ awaygo/settings.py
@@ -18,7 +18,7 @@ env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
-
+# reading .env file
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,7 +38,6 @@ DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['awaygo2.herokuapp.com', '127.0.0.1']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -51,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -155,5 +155,13 @@ MEDIA_URL = '/media/'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
+
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'awaygo'
+AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL=None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
