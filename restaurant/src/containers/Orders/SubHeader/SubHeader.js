110f544d753937788dbb0299259d2bc9d3bd1852
Yariv <shenkar7@gmail.com>
2021-05-23 13:53:53 +0300

    removing unsecured font

---
+++ restaurant/src/containers/Orders/SubHeader/SubHeader.js
@@ -0,0 +1,38 @@
+import './SubHeader.css';
+import addImg from '../../../assets/img/add.png'
+import addHoverImg from '../../../assets/img/addHover.png'
+import React, {useState} from 'react';
+
+const SubHeader = props => {
+    
+    const [addHover, setAddHover] = useState(false);
+
+    return (
+        <div className="sub-header"> 
+            <div className="add-order-button">
+                {addHover
+                    ? <img src={addHoverImg} alt="hoverAdd" onMouseOut={() => setAddHover(false)} onClick={props.addOrderHandler} />
+                    : <img src={addImg} alt="add" onMouseOver={() => setAddHover(true)} />
+                }
+            </div>
+            <div className="navigation">
+                <span className="navigation-button" onClick={props.historyHandler}>
+                    <i class="fas fa-history"></i>
+                    היסטורית הזמנות
+                </span>
+                <span className="separator">|</span>
+                <span className="navigation-button" onClick={props.menuSiteClosureHandler}>
+                    <i class="fas fa-store-slash"></i>
+                    השבתת אתר התפריט
+                </span>
+                <span className="separator">|</span>
+                <span className="navigation-button" onClick={props.dishesVisibilityHandler}>
+                    <i class="fas fa-eye-slash"></i>
+                    הסתרת/הצגת מנות
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export default SubHeader;
