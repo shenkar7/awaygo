fcd013e8c98c18d6a9d00da6d71f0aa0336ce9db
Yariv <shenkar7@gmail.com>
2021-06-06 23:56:29 +0300

    updating SECURE_HSTS_SECONDS

---
+++ restaurant-menu/src/containers/CustomerInfo/CustomerInfo.js
@@ -0,0 +1,312 @@
+import './CustomerInfo.css';
+import React, {useContext, useState} from 'react';
+import OrderContext from '../../OrderContext';
+import Spinner from '../../components/Spinner/Spinner';
+import axios from 'axios';
+
+const CustomerInfo = props => {
+
+    const [order, setOrder] = useContext(OrderContext);
+
+    const [status, setStatus] = useState(null);
+
+    // valid only when all are true. updating in realtime on each onChange()
+    const [validation, setValidation] = useState(
+        {
+            phone_number: false,
+            email: true,    // not a required field, so true as default
+            first_name: false,
+            last_name: false,
+            city: false,
+            street: false,
+            number: false,
+            apartment: true // not a required field, so true as default
+        }
+    );
+    
+    // updating on submit (to color the unvalid fields in red)
+    const [submitValidation, setSubmitValidation] = useState(
+        {
+            phone_number: true,
+            email: true,
+            first_name: true,
+            last_name: true,
+            city: true,
+            street: true,
+            number: true,
+            apartment: true
+        }
+    );
+
+    const [invalidMessage, setInvalidMessage] = useState(null);
+
+    const allValid = () => {
+        for (let key in validation) {
+            if (!validation[key])
+                return false;
+        }
+        return true;
+    }
+
+    const onChangeHandler = (event, fieldName) => {
+        const newOrder = {...order};
+        newOrder.customer = {...order.customer};
+        const newValidation = {...validation};
+
+        if (fieldName === "phone_number"){
+            newOrder.customer.phone_number = event.target.value;
+            newValidation.phone_number = true;
+            const regex = /^[0-9]+$/;
+            if (!regex.test(event.target.value))
+                newValidation.phone_number = false;
+            else if (event.target.value.length !== 10)
+                newValidation.phone_number = false;
+            else if (event.target.value.substring(0, 2) !== "05")
+                newValidation.phone_number = false;
+        }
+
+        if (fieldName === "email"){
+            newOrder.customer.email = event.target.value;
+            newValidation.email = true;
+            if ((!event.target.value.includes("@") || !event.target.value.includes(".")) && !(event.target.value === null || event.target.value === ""))
+                newValidation.email = false;
+        }
+
+        if (["first_name", "last_name"].includes(fieldName)){
+            newOrder.customer[fieldName] = event.target.value;
+            newValidation[fieldName] = true;
+            if (event.target.value === null || event.target.value === "")
+                newValidation[fieldName] = false;
+        }
+
+        if (["city", "street"].includes(fieldName)){
+            newOrder[fieldName] = event.target.value;
+            newValidation[fieldName] = true;
+            if (event.target.value === null || event.target.value === "")
+                newValidation[fieldName] = false;
+        }
+
+        if (fieldName === "number"){
+            newOrder.number = event.target.value;
+            newValidation.number = false;
+            if (event.target.value > 0 )
+                newValidation.number = true;
+        }
+
+        if (fieldName === "apartment"){
+            newOrder.apartment = event.target.value;
+            newValidation.apartment = true;
+
+            if (event.target.value <= 0 && !(event.target.value === null || event.target.value === ""))
+                newValidation.apartment = false;
+        }
+
+        if (fieldName === "remark")
+            newOrder.remark = event.target.value;
+
+        setOrder(newOrder);
+        setValidation(newValidation);
+    }
+
+    const transformOrderToIds = order => {
+        order.dishes_in_order.forEach(dishInOrder => {
+            dishInOrder.dish = dishInOrder.dish.id;
+            for(let i=0; i < dishInOrder.extras.length; i++) {
+                dishInOrder.extras[i] = dishInOrder.extras[i].id;
+            }
+        });
+        return order;
+    }
+
+    const getCookie = name => {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+
+    const csrftoken = getCookie('csrftoken');
+
+    let originURL = window.location.origin;
+    //originURL = "http://127.0.0.1:8000"; // Only for local run
+
+    const submitHandler = event => {
+        event.preventDefault();
+        
+        if (allValid()){
+            setStatus("loading");
+            const orderWithIds = transformOrderToIds(order);
+            axios.post(originURL + '/order_add', orderWithIds,
+                {
+                    headers: {'X-CSRFTOKEN': csrftoken,},
+                },
+            )
+            .then(res => {
+                console.log("SUCCESS adding order")
+                props.setPage({page: "success", info: res.data.id})
+            })
+            .catch(err => {
+                console.log("ERROR posting order");
+                console.log(err.message);
+                setStatus("error");
+            })
+        }
+        else{
+            setSubmitValidation(validation);
+            setInvalidMessage(
+                <div className="invalidMessage">* נא לתקן פרטים המסומנים באדום</div>
+            );
+        }
+    }
+
+    let content = null;
+    if (!status) {
+        content = (
+            <React.Fragment>
+                <div className="back">
+                    <span onClick={props.back}>חזור לתפריט</span>
+                </div>
+                <div className="form-window">
+                    <form onSubmit={submitHandler}>
+
+                        <h3>פרטי המזמין</h3>
+                        <div className="details-section">
+                            <div className="input-couple">
+                                <div className={submitValidation.phone_number ? "info" : "info invalid"}>
+                                    <label>* טלפון נייד</label>
+                                    <input
+                                        type="tel"
+                                        placeholder="טלפון נייד"
+                                        value={order.customer.phone_number}
+                                        onChange={event => onChangeHandler(event, "phone_number")}
+                                    />
+                                </div>
+                                <div className="seperator"></div>
+                                <div className="info">
+                                    <label>אימייל</label>
+                                    <input
+                                        type="email"
+                                        placeholder="אימייל"
+                                        value={order.customer.email}
+                                        onChange={event => onChangeHandler(event, "email")}
+                                    />
+                                </div>
+                            </div>
+                            <div className="seperator"></div>
+                            <div className="input-couple">
+                                <div className={submitValidation.first_name ? "info" : "info invalid"}>
+                                    <label>* שם פרטי</label>
+                                    <input
+                                        type="text"
+                                        placeholder="שם פרטי"
+                                        value={order.customer.first_name}
+                                        onChange={event => onChangeHandler(event, "first_name")}
+                                    />
+                                </div>
+                                <div className="seperator"></div>
+                                <div className={submitValidation.last_name ? "info" : "info invalid"}>
+                                    <label>* שם משפחה</label>
+                                    <input
+                                        type="text"
+                                        placeholder="שם משפחה"
+                                        value={order.customer.last_name}
+                                        onChange={event => onChangeHandler(event, "last_name")}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <h3>כתובת</h3>
+                        <div className="details-section">
+                            <div className="input-couple">
+                                <div className={submitValidation.city ? "info" : "info invalid"}>
+                                    <label>* עיר</label>
+                                    <input
+                                        type="text"
+                                        placeholder="עיר"
+                                        value={order.city}
+                                        onChange={event => onChangeHandler(event, "city")}
+                                    />
+                                </div>
+                                <div className="seperator"></div>
+                                <div className={submitValidation.street ? "info" : "info invalid"}>
+                                    <label>* רחוב</label>
+                                    <input
+                                        type="text"
+                                        placeholder="רחוב"
+                                        value={order.street}
+                                        onChange={event => onChangeHandler(event, "street")}
+                                    />
+                                </div>
+                            </div>
+                            <div className="seperator"></div>
+                            <div className="input-couple">
+                                <div className={submitValidation.number ? "info" : "info invalid"}>
+                                    <label>* מספר בית</label>
+                                    <input
+                                        type="number"
+                                        min="1" 
+                                        placeholder="מספר בית"
+                                        step="1"
+                                        value={order.number}
+                                        onChange={event => onChangeHandler(event, "number")}
+                                    />
+                                </div>
+                                <div className="seperator"></div>
+                                <div className="info">
+                                    <label>מספר דירה</label>
+                                    <input
+                                        type="number"
+                                        min="1"
+                                        placeholder="מספר דירה"
+                                        value={order.apartment}
+                                        onChange={event => onChangeHandler(event, "apartment")}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+                        <h3>הערות להזמנה</h3>
+                        <div className="remark-section">
+                            <input
+                                type="text"
+                                placeholder="הערות"
+                                value={order.remark}
+                                onChange={event => onChangeHandler(event, "remark")}
+                            />
+                        </div>
+                        <div className="submit-section">
+                            <button type="submit">בצע הזמנה</button>
+                        </div>
+                        {invalidMessage}
+                    </form>
+                </div>
+            </React.Fragment>
+        );
+    }
+    else if(status === "loading")
+        content = (
+            <div className="spinner-and-error">
+                <Spinner/>
+            </div>
+        );
+    else if(status === "error")
+        content = (
+            <div className="spinner-and-error">
+                <p>שגיאת תקשורת, נא לנסות מאוחר יותר</p>
+            </div>
+        );
+
+    return content;
+}
+
+export default CustomerInfo;
---
+++ restaurant/src/containers/Orders/NewOrder/NewOrder.js
@@ -0,0 +1,126 @@
+import './NewOrder.css';
+import React, {useState} from 'react';
+import Spinner from '../../../components/Spinner/Spinner';
+import TimingOrder from '../../../components/TimingOrder/TimingOrder';
+import {getCookie} from '../../../assets/functions';
+import axios from 'axios';
+
+const NewOrder = props => {
+
+    const [status, setStatus] = useState(null);
+    
+    const timingButtonHandler = (timing) => {
+        const newOrder = {...props.order};
+
+        newOrder.process_date_time = new Date(Date.now() + timing * 60 * 1000);
+        newOrder.status = 'process';
+        newOrder.customer = newOrder.customer.id;
+
+        setStatus('loading');
+        const originURL = window.location.origin;
+        const csrftoken = getCookie('csrftoken');
+
+        axios.put(originURL + '/order/' + newOrder.id + '/',
+            newOrder,
+            {
+                headers: {'X-CSRFTOKEN': csrftoken,},
+            },
+        )
+        .then(res => {
+            console.log('SUCCESS updating order');
+            newOrder.customer = props.order.customer;
+            setStatus(null);
+            props.orderUpdateHandler(newOrder);
+            props.modalClose(false);
+        })
+        .catch(err => {
+            setStatus('error');
+            console.log('ERROR updating order');
+            console.log(err.message);
+        })
+
+    }
+
+    let content = null;
+    let timingOrder = null;
+
+    if(status === 'error'){
+        content = (
+            <div className="new-order-window error">
+                שגיאת תקשורת
+            </div>
+        );
+    }
+    else {
+        if (status === 'loading'){
+            timingOrder = <span className='spinner'><Spinner/></span>;
+        }
+        else {
+            timingOrder = (
+                <TimingOrder timingButtonHandler={timingButtonHandler}/>
+            );
+        }
+
+        let street, number, apartment;
+        street = number = apartment = '';
+    
+        if(props.order.street)
+            street = " " + props.order.street;
+        if(props.order.number)
+            number = " " + String(props.order.number);
+        if(props.order.apartment)
+            apartment = " דירה " + String(props.order.apartment);
+
+        content = (
+            <div className="new-order-window">
+                <div className="order-number"><b>#{props.order.id}</b></div>
+                <div className="inside-window">
+                    <div className="name">{props.order.customer.first_name + " " + props.order.customer.last_name}</div>
+                    <div className="address">{props.order.city + street + number + apartment}</div>
+                    <hr/>
+                    <div className="food-section">
+                        <i className="fas fa-hamburger"></i>
+                        <div>
+                            {props.order.dishesinorder.map(dishInOrder => {
+                                let extras = [];
+                                for (let i = 0; i < dishInOrder.extras.length; i++) {
+                                    if (i < dishInOrder.extras.length - 1)
+                                        extras.push(dishInOrder.extras[i].name + ", ");
+                                    else
+                                        extras.push(dishInOrder.extras[i].name);
+                                }
+                                let remark = null;
+                                if (dishInOrder.remark)
+                                    remark = <p className="food">{dishInOrder.remark}{" "}<i className="fas fa-exclamation-triangle"></i></p>
+                                return (
+                                    <div key={Math.random()}>
+                                        {dishInOrder.dish.name + " × " + dishInOrder.quantity}
+                                        <p className="food">{extras}</p>
+                                        {remark}
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                    {props.order.remark ?
+                    <React.Fragment>
+                        <hr/>
+                        <div className="remark-section">
+                            <i className="fas fa-exclamation-triangle"></i>
+                            <div>
+                                <p className="remark">{props.order.remark}</p>
+                            </div>
+                        </div>
+                    </React.Fragment>
+                    : null   
+                    }
+                </div>
+                {timingOrder}
+            </div>
+        );
+    }
+
+    return content;
+}
+
+export default NewOrder;
